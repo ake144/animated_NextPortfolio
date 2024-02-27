6505e8dd34f3621db07bca70765ae28bbb78ed3c
ake144 <tamiratake@gmail.com>
2024-02-27 14:00:13 +0300

    modified:   src/app/blog/page.jsx

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -43,15 +43,15 @@ const Blog = () => {
           >
             <div className="flex flex-col gap-3 text-white">
               <h1 className="text-xl  font-bold md:text-2xl lg:text-2xl xl:text-3xl text-black">
-                {item.title}
+                {item?.title}
               </h1>
               <div className="relative w-30 h-26 md:w-66 md:h464 lg:w-[200px] lg:h-90px] xl:w-[200px] xl:h-[120px]">
-                <Image src={item.img} alt="png " fill />
+                <Image src={item?.img} alt="png " fill />
               </div>
               <p className="w-50 md:w-76 lg:w-[200px] lg:text-md xl:w-[300px] text-black">
-                {item.desc}
+                {item?.desc}
               </p>
-              <Link href={item.link} passHref>
+              <Link href={item?.link} passHref>
                 <button className="p-5 text-sm md:p-4 md:text-md lg:p-4 lg:text-md bg-white text-gray-600 font-semibold m-4 rounded">
                   See Demo
                 </button>
